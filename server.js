dd2816286e28ce173ecfea978fe6368f51db708a
IdamEniIdam <idameni89@gmail.com>
2018-09-15 17:07:04 +1300

    Add project page

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get('/bad', (req,res) => {
     })
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+      pageTitle: 'Projects'
+        });
+}); 
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
